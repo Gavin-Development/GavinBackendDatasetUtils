519dbd048d3a45c95aeda40346878c318b17f096
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-10-10 17:34:28 +0100

    Adjust for easier debugging.

--- test.py
+++ test.py
@@ -6,5 +6,7 @@ sys.path.append(str(root_path))
 
 import GavinBackendDatasetUtils as LTD
 
+print("Starting Single Thread: ")
 samples_one = LTD.LoadTrainDataST(800, "./", "Test.BIN", 69108, 66109, 52, 0)
+print("Starting Multi Thread: ")
 samples_two = LTD.LoadTrainDataMT(800, "./", "Test.BIN", 69108, 66109, 52, 0)
