cc09b3df9121765d3d3cba815a0cac2a27d89108
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-12-04 14:19:13 +0000

    Update files from master

---
+++ GavinBackendDatasetUtils/Tokenizer.cpp
@@ -0,0 +1,31 @@
+#include "DataLoader.hpp"
+
+Tokenizer::Tokenizer(std::string iTokenizerName, uint64_t iVocabSize) {
+	std::cout << "Initialising a new tokenizer." << std::endl;
+	TokenizerName = iTokenizerName;
+	MaxVocabSize = iVocabSize;
+};
+
+Tokenizer::Tokenizer(std::string iTokenizerPath) {
+	std::cout << "Loading existing tokenizer." << std::endl;
+};
+
+void Tokenizer::Tokenize(std::vector<std::string> Samples) {
+	bool EncodingExists;
+
+	for (std::string Word : Samples) {
+		EncodingExists = false;
+		for (int i = 0; i < Encodings.size(); i++) {
+			if (Encodings[i] == Word) {
+				Commonality[i]++;
+				EncodingExists = true;
+				break;
+			}
+		}
+
+		if (EncodingExists == false && Encodings.size() < MaxVocabSize) {
+			Encodings.push_back(Word);
+			Commonality.push_back(1);
+		}
+	}
+};
