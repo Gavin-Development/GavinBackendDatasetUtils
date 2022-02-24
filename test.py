f4bb690c0cfbd719373c8cec6865a90553e03ef7
scot <40865296+Scot-Survivor@users.noreply.github.com>
2022-02-24 21:59:20 +0000

    [FIX] Add test files
    + Test.BIN: Small test file
    + test.py: Small python file to test if the module works.
    
    Scot-Survivor

---
+++ test.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+root_path = Path(__file__).resolve()
+sys.path.append(str(root_path))
+
+import GavinBackendDatasetUtils as LTD
+
+print(LTD.__dict__)
+print(LTD.__version__)
+
+print("Starting Single Thread: ")
+samples_one = LTD.LoadTrainDataST(800, "./", "Test.BIN", 69108, 66109, 52, 0)
+print("Starting Multi Thread: ")
+samples_two = LTD.LoadTrainDataMT(800, "./", "Test.BIN", 69108, 66109, 52, 0)
