9c1f440aee8d601ca112b06a30fad39658d992fd
scot <40865296+Scot-Survivor@users.noreply.github.com>
2022-03-06 22:57:33 +0000

    [IMPROVEMENT/FEATURE] Add testing
    + Add GTest to dependencies
    + Add unittests.cpp
    
    Scot-Survivor

---
+++ test/unittests.cpp
@@ -0,0 +1,120 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include <DataLoader.hpp>
+
+std::list<std::string> load_text(std::string filename) {
+    std::list<std::string> texts;
+    std::ifstream input(filename);
+    std::string lineBuffer;
+    while (std::getline(input, lineBuffer)) {
+        texts.push_back(lineBuffer);
+    }
+    return texts;
+};
+
+
+class TokenizerTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        texts = load_text("test/unittests.cpp");
+        TestTokenizer.build_vocab(texts);
+    }
+
+    virtual void TearDown() {
+        texts.clear();
+        delete &TestTokenizer, &EmptyTokenizer;
+    }
+
+    std::list<std::string> texts;
+    std::string name = "Test";
+    uint64_t vocab_size = 1000;
+    Tokenizer EmptyTokenizer = Tokenizer(name, vocab_size);
+    Tokenizer TestTokenizer = Tokenizer(name+"-filled", vocab_size);
+    // TODO: setup encode/decode
+};
+
+
+TEST_F(TokenizerTest, intialised_correctly) {
+    EXPECT_EQ(EmptyTokenizer.get_name(), name);
+    EXPECT_EQ(EmptyTokenizer.get_vocab_size(), vocab_size);
+    EXPECT_EQ(TestTokenizer.get_name(), name+"-filled");
+}
+
+TEST_F(TokenizerTest, test_build_vocab) {
+    EXPECT_EQ(TestTokenizer.get_vocab_size(), vocab_size);
+    unsigned int vocab_length = TestTokenizer.get_vocab().size();
+    int error = ceil((double)vocab_length*0.1); // 10% error
+    EXPECT_NEAR(vocab_length, vocab_size, error);
+}
+
+
+TEST(DataLoaders, multi_threaded_loading) {
+    auto data = LoadTrainDataMT(800, "./",
+                                 "Test.BIN", 69108,
+                                 66109, 52, 0);
+    EXPECT_EQ(data.shape()[0], 800);
+    EXPECT_EQ(data.shape()[1], 52);
+
+    std::list<int> start_tokens;
+    for (int i = 0; i < 800; ++i) {
+        start_tokens.push_back(data.at(i, 0));
+    }
+    EXPECT_TRUE(std::all_of(start_tokens.begin(), start_tokens.end(),
+                            [](int i) { return i == 69108; }));
+
+    auto data2 = LoadTrainDataMT(800, "./",
+                                 "Test.BIN", 69108,
+                                 66109, 52, 0);
+    EXPECT_EQ(data2.shape(), data.shape());
+}
+
+
+TEST(DataLoaders, single_threaded_loading) {
+    auto data = LoadTrainDataST(800, "./",
+                                "Test.BIN", 69108,
+                                66109, 52, 0);
+    EXPECT_EQ(data.shape()[0], 800);
+    EXPECT_EQ(data.shape()[1], 52);
+
+    std::list<int> start_tokens;
+    for (int i = 0; i < 800; ++i) {
+        start_tokens.push_back(data.at(i, 0));
+    }
+    EXPECT_TRUE(std::all_of(start_tokens.begin(), start_tokens.end(),
+                            [](int i) { return i == 69108; }));
+
+    auto data2 = LoadTrainDataST(800, "./",
+                                 "Test.BIN", 69108,
+                                 66109, 52, 0);
+    EXPECT_EQ(data2.shape(), data.shape());
+}
+
+TEST(DataLoaders, legacy_loading) {
+    auto data = LoadTrainDataST_Legacy(800, "./",
+                                "small.to", 69108,
+                                66109, 52, 0);
+    EXPECT_EQ(data.size(), 800);
+    std::list<int> lengths;
+    for (const auto& row: data) {
+        lengths.push_back(row.size());
+    }
+    EXPECT_TRUE(std::all_of(lengths.begin(), lengths.end(),
+                            [](int i) { return i == 52; }));
+
+    auto data2 = LoadTrainDataST_Legacy(800, "./",
+                                 "small.to", 69108,
+                                 66109, 52, 0);
+    EXPECT_EQ(data2.size(), data.size());
+    std::list<int> lengths2;
+    for (const auto& row: data2) {
+        lengths2.push_back(row.size());
+    }
+    EXPECT_TRUE(std::all_of(lengths2.begin(), lengths2.end(),
+                            [](int i) { return i == 52; }));
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
