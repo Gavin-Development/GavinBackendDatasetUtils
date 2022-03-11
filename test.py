7d1e762fa85fdcec8bf33360b5c309bcfba8a383
scot <40865296+Scot-Survivor@users.noreply.github.com>
2022-03-11 21:36:19 +0000

    [FEATURE] Expose encode/decode functions to python
    + Add encode / decode to test.py
    
    Scot-Survivor

--- test.py
+++ test.py
@@ -8,22 +8,8 @@ sys.path.append(str(root_path))
 import GavinBackendDatasetUtils as LTD
 
 
-test_vocab = """A short story is a piece of prose fiction that
-    typically can be read in one sitting and focuses
-    on a self-contained incident or series of linked
-    incidents, with the intent of evoking a single
-    effect or mood. Tokenization, when applied to data security,
-    is the process of substituting a sensitive
-    data element with a non-sensitive equivalent,
-    referred to as a token, that has no extrinsic
-    or exploitable meaning or value. The token is
-    a reference that maps back to the sensitive data
-    through a tokenization system."""
-
-sentences = test_vocab.split('.')
-sentences = [s.strip() for s in sentences]
-sentences = [s for s in sentences if len(s) > 0]
-sentences = [s + '.' for s in sentences]
+handle = open("test-lines.txt", "r")
+test_vocab = [line for line in handle.readlines()]
 
 
 print(LTD.__dict__)
@@ -38,7 +24,11 @@ print(array_d.shape)
 
 tokenizer = LTD.Tokenizer("Test", 500)
 print("Building tokenizer values...")
-tokenizer.build_vocab(sentences)
+tokenizer.build_vocab(test_vocab)
 print("Tokenizer values: ")
 print(f"Vocab Size: {tokenizer.get_vocab_size()}")
 print(f"Vocab: {tokenizer.get_vocab()}")
+encoded = tokenizer.encode("This is a test")
+decoded = tokenizer.decode(encoded)
+print(f"Encoded: {encoded}")
+print(f"Decoded: {decoded}")
