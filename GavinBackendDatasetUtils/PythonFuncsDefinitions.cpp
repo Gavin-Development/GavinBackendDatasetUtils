1632b13066feda045714648c152fef4dbf0e3052
Scot_Survivor <unconfigured@null.spigotmc.org>
2022-10-18 22:45:57 +0100

    Improvement
    Change python function definitions to match standard Python naming. Unlike other langauges Python tends to use snake_case for methods & functions. Whilst using PascalCase for class names.

--- GavinBackendDatasetUtils/PythonFuncsDefinitions.cpp
+++ GavinBackendDatasetUtils/PythonFuncsDefinitions.cpp
@@ -10,30 +10,30 @@ int InitModule() {
 PYBIND11_MODULE(GavinBackendDatasetUtils, handle) {
     InitModule();
     handle.doc() = "This module is a custom module written in c++ to accelerate dataset loading for Gavin Bot made by Josh (Scot_Survivor)";
-    handle.def("LoadTrainDataMT", &LoadTrainDataMT);
-    handle.def("LoadTrainDataST", &LoadTrainDataST);
+    handle.def("load_train_data_mt", &LoadTrainDataMT);
+    handle.def("load_train_data_st", &LoadTrainDataST);
 
     py::class_<Tokenizer>(handle, "Tokenizer")
         .def(py::init<std::string>())
         .def(py::init<>())
 
-        .def_readonly("Words", &Tokenizer::Encodings)
-        .def_readonly("Occurrences", &Tokenizer::Commonalities)
-        .def("Save", static_cast<bool (Tokenizer::*)(void)>(&Tokenizer::SaveTokenizer))
-        .def("Save", static_cast<bool (Tokenizer::*)(std::string)>(&Tokenizer::SaveTokenizer))
-        .def("Load", static_cast<bool (Tokenizer::*)(void)>(&Tokenizer::LoadTokenizer))
-        .def("Load", static_cast<bool (Tokenizer::*)(std::string)>(&Tokenizer::LoadTokenizer))
-        .def("Encode", &Tokenizer::Encode)
-        .def("GetVocabSize", &Tokenizer::GetVocabSize)
-        .def("Decode", &Tokenizer::Decode)
-        .def("BuildEncodes_GPU", &Tokenizer::BuildEncodes_GPU)
-        .def("BuildEncodes", &Tokenizer::BuildEncodes);
+        .def_readonly("words", &Tokenizer::Encodings)
+        .def_readonly("occurrences", &Tokenizer::Commonalities)
+        .def("save", static_cast<bool (Tokenizer::*)(void)>(&Tokenizer::SaveTokenizer))
+        .def("save", static_cast<bool (Tokenizer::*)(std::string)>(&Tokenizer::SaveTokenizer))
+        .def("load", static_cast<bool (Tokenizer::*)(void)>(&Tokenizer::LoadTokenizer))
+        .def("load", static_cast<bool (Tokenizer::*)(std::string)>(&Tokenizer::LoadTokenizer))
+        .def("encode", &Tokenizer::Encode)
+        .def("get_vocab_size", &Tokenizer::GetVocabSize)
+        .def("decode", &Tokenizer::Decode)
+        .def("build_encodes_GPU", &Tokenizer::BuildEncodes_GPU)
+        .def("build_encodes", &Tokenizer::BuildEncodes);
 
     py::class_<BINFile>(handle, "BINFile")
         .def(py::init<std::string, int, int, int, int>())
         .def(py::init<std::string, int, int, int, int, int>())
-        .def_readonly("NumberOfSamples", &BINFile::NumberOfSamplesInFile)
-        .def_readonly("MaxNumberOfSamples", &BINFile::MaxNumberOfSamples)
+        .def_readonly("number_of_samples", &BINFile::NumberOfSamplesInFile)
+        .def_readonly("max_number_of_samples", &BINFile::MaxNumberOfSamples)
         .def("get_slice", &BINFile::get_slice)
         .def("append", &BINFile::append)
         .def("__getitem__", static_cast<py::array_t<int>(BINFile::*)(uint64_t)>(&BINFile::operator[]));
